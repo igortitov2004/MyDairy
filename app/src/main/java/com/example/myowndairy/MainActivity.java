efac92938b9215884361bd2aaf8313f8d69bfb9e
igorek <igor-titov-2004@mail.ru>
2023-05-31 10:04:37 +0300

    Create NavigationBar

--- app/src/main/java/com/example/myowndairy/MainActivity.java
+++ app/src/main/java/com/example/myowndairy/MainActivity.java
@@ -3,11 +3,41 @@ package com.example.myowndairy;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.myowndairy.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(R.id.homeMainBottom==item.getItemId()){
+                replaceFragment(new HomeFragment());
+            }
+            if(R.id.calendarMainBottom==item.getItemId()){
+                replaceFragment(new CalendarFragment());
+            }
+            if(R.id.settingsMainBottom==item.getItemId()){
+                replaceFragment(new SettingsFragment());
+            }
+
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
