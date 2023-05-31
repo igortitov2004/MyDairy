c757b550fcb802b159cce7a6a5b87ec2db55234e
Saveliy Komlenok <skomlenok363@gmail.com>
2023-05-31 19:45:55 +0300

    Update navigation bar

--- app/src/main/java/com/example/myowndairy/MainActivity.java
+++ app/src/main/java/com/example/myowndairy/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myowndairy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -8,36 +11,38 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.myowndairy.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+        MenuItem startItem = binding.bottomNavigationView.getMenu().findItem(R.id.homeMainBottom);
+        startItem.setChecked(true);
 
-            if(R.id.homeMainBottom==item.getItemId()){
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(R.id.homeMainBottom == item.getItemId()){
                 replaceFragment(new HomeFragment());
             }
-            if(R.id.calendarMainBottom==item.getItemId()){
+            if(R.id.calendarMainBottom == item.getItemId()){
                 replaceFragment(new CalendarFragment());
             }
-            if(R.id.settingsMainBottom==item.getItemId()){
+            if(R.id.settingsMainBottom == item.getItemId()){
                 replaceFragment(new SettingsFragment());
             }
-
-
             return true;
         });
     }
     private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout,fragment);
         fragmentTransaction.commit();
     }
